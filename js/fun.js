a3d238d8db36c9c67e5a70c3ecb18f52f7dbf08d
Tyler J. Lubeck <tyler.lubeck@tufts.edu>
2014-01-09 15:27:39 -0500

    The distance I'll take a joke...

---
+++ js/fun.js
@@ -0,0 +1,11 @@
+function canadia() {
+    $('#canada').on('mouseenter', function() {
+        $('#flag').show();
+        var song = document.getElementById('canadaSong');
+        song.play()
+    }).on('mouseleave', function() {
+        $('#flag').hide();
+        var song = document.getElementById('canadaSong');
+        song.pause();
+    });
+}
